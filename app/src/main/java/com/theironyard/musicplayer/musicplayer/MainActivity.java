c5c677414f41bbf89adf316249e1fc06f94cbe46
Krista Blachian <KristaBlachian@outlook.com>
2018-08-16 01:36:24 -0400

    everything seems to be in place but music doesn't play

--- app/src/main/java/com/theironyard/musicplayer/musicplayer/MainActivity.java
+++ app/src/main/java/com/theironyard/musicplayer/musicplayer/MainActivity.java
@@ -1,13 +1,85 @@
 package com.theironyard.musicplayer.musicplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private MediaPlayer mediaPlayer;
+    private ImageView artistImage;
+    private TextView leftTime;
+    private TextView rightTime;
+    private SeekBar seekBar;
+    private Button prevButton;
+    private Button playButton;
+    private Button nextButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpUI();
+
+        }
+
+    public void setUpUI() {
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.level2copy);
+        artistImage = (ImageView) findViewById(R.id.imageView);
+        leftTime = (TextView) findViewById(R.id.leftTimeID);
+        rightTime = (TextView) findViewById(R.id.rightTimeID);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        prevButton = (Button) findViewById(R.id.prevButton);
+        playButton = (Button) findViewById(R.id.playButton);
+        nextButton = (Button) findViewById(R.id.nextButton);
+
+        prevButton.setOnClickListener(this);
+        playButton.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+       switch (v.getId()) {
+           case R.id.prevButton:
+               //code
+               break;
+
+           case R.id.playButton:
+               if (mediaPlayer.isPlaying()) {
+                   pauseMusic();
+               }else {
+                   startMusic();
+               }
+
+               break;
+
+           case R.id.nextButton:
+
+               break;
+       }
+    }
+    public void pauseMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+            playButton.setBackgroundResource(android.R.drawable.ic_media_play);
+        }//if playing a song, change play icon to pause icon
+
+    }
+    public void startMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+            playButton.setBackgroundResource(android.R.drawable.ic_media_pause);
+
+        }
+
     }
 }
